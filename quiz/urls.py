1d24607eec0545bd1c5eaaeb08e6ee8afbf451a3
Mariia Troilina <troilina.mv@phystech.edu>
2026-04-14 20:25:00 +0500

    add scores page

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('questions/', views.questions_list, name='questions_list'),
-    path('add_question/', views.add_question, name='add_question')
+    path('add_question/', views.add_question, name='add_question'),
+    path('take_test/', views.take_test, name='take_test'),
+    path('scores/', views.scores, name='scores'),
 ]
